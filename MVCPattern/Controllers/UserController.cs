14c754a7fefde05e67c0ea7c0a6edcbfbeecd496
Trashvin <m3lles@gmail.com>
2016-06-17 12:38:27 -0700

    Added MVC Pattern

---
+++ MVCPattern/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataProviderService;
+using MVCPattern.Models;
+
+namespace MVCPattern.Controllers
+{
+    public class UserController : Controller
+    {
+
+        static UserDBProvider db = new UserDBProvider();
+        //
+        // GET: /User/
+
+
+        public ActionResult Index()
+        {
+            List<Models.UserModel> users = new List<Models.UserModel>();
+
+            foreach(MyUser rawUser in db.GetUsers())
+            {
+                users.Add(new Models.UserModel(rawUser));
+            }
+
+            return View(users);
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            Models.UserModel user = new Models.UserModel(db.FindUserByID(id));
+
+            if (user == null) return HttpNotFound();
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult EditUser(UserModel model)
+        {
+            
+            if ( db.EditUser(model.ToMyUser()) )
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+        
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Models.UserModel user = new Models.UserModel(db.FindUserByID(id));
+
+            if (user == null) return HttpNotFound();
+            
+            return View(user);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteUser(int id)
+        {
+            db.DeleteUser(id);
+
+            return RedirectToAction("index");
+        }
+            
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create(UserModel model)
+        {
+            if ( db.AddUser(model.ToMyUser()))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+}
---
+++ MVCPattern/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataProviderService;
+
+namespace MVCPattern.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public UserModel()
+        {
+
+        }
+
+        public UserModel(DataProviderService.MyUser aUser)
+        {
+            UserID = aUser.UserID;
+            FullName = aUser.FullName;
+            Gender = aUser.Gender;
+            Age = aUser.Age;
+        }
+
+        public MyUser ToMyUser()
+        {
+            MyUser temp = new MyUser() { UserID = this.UserID, FullName = this.FullName, Gender = this.Gender, Age = this.Age };
+
+            return temp;
+        }
+    }
+}
