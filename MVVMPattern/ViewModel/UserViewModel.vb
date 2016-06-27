c502d9d7a6c34bce6483e377743b20bd5f9361a3
Trashvin <m3lles@gmail.com>
2016-06-27 05:14:25 -0700

    Added the solution of adding new user in the MVVM Pattern

--- MVVMPattern/ViewModel/UserViewModel.vb
+++ MVVMPattern/ViewModel/UserViewModel.vb
@@ -12,6 +12,7 @@ Public Class UserViewModel
     Private _db As New UserDBProvider()
     Private _selectedUser As New UserModel()
     Private _usersList As New ObservableCollection(Of UserModel)
+
     Private _editCommand As ICommand
     Private _deleteCommand As ICommand
     Private _addCommand As ICommand
@@ -123,21 +124,59 @@ Public Class UserViewModel
         End Set
     End Property
 
+    Public Property AddCommand As ICommand
+        Get
+            If (_addCommand Is Nothing) Then
+                _addCommand = New RelayCommand(Sub(param) Me.Add(), Nothing)
+            End If
+
+            Return _addCommand
+        End Get
+        Set(value As ICommand)
+            _addCommand = value
+        End Set
+    End Property
+
     Private Sub Edit()
         EditMode = True
         AddMode = False
         Editable = True
-      
+
     End Sub
 
     Private Sub Save()
         Editable = False
 
-        _db.EditUser(SelectedUser.ToMyUser())
+        If AddMode Then
+            _db.AddUser(SelectedUser.ToMyUser())
+        Else
+            _db.EditUser(SelectedUser.ToMyUser())
+        End If
+
+
         SelectedUser = _usersList(0)
 
         EditMode = False
         AddMode = False
+
+    End Sub
+
+    Private Sub Add()
+        Editable = True
+        EditMode = True
+        AddMode = True
+
+        'we can compute for the next userID and create a new instance of UserModel for the new user
+        Dim newUser As New UserModel() With {.UserID = GetNextUserID()}
+
+        'add the new temp user to the list
+        UsersList.Add(newUser)
+
+        'set the new user as the selected user
+        SelectedUser = UsersList(UsersList.Count - 1)
+
+        'Note : The solution does not support the cancellationof Add. Thus, the new user is always have to be saved.
+        'If the cancellation is to be supported, this temp user have to be remved from the list
 
     End Sub
 
@@ -152,6 +191,21 @@ Public Class UserViewModel
             SelectedUser = _usersList(0)
         End If
     End Sub
+
+    Private Function GetNextUserID() As Integer
+        'This is just to easily generate a valid UserID, it not a recommended solution
+        'for big projects. Try persisiting the last generated ID on a file/db to keep track.
+
+        Dim maxID As Integer = 0
+
+        For Each rawUser As UserModel In _usersList
+            If (rawUser.UserID > maxID) Then
+                maxID = rawUser.UserID
+            End If
+        Next
+
+        GetNextUserID = maxID + 1
+    End Function
 
     Private Sub NotifyPropertyChanged(ByVal propertyName As String)
         RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
